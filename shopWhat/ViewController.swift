d3d3d0b8a0977ef4d8d9ca07fc4a189ba30cb3a2
youngstudent2 <644699280@qq.com>
2020-11-09 22:10:49 +0800

    a part of ui

--- shopWhat/ViewController.swift
+++ shopWhat/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var product_name: UILabel!
+    @IBOutlet weak var name_text: UITextField!
+    @IBOutlet weak var photo_image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        name_text.delegate = self
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        product_name.text = textField.text
+        print("123")
+    }
+    
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        print("tap to select image")
+        // Hide the keyboard
+        /*
+        name_text.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)*/
+    }
 
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following:\(info)")
+        }
+        photo_image.image = selectedImage
+        dismiss(animated: true, completion: nil)
+        
+    }
 }
 
